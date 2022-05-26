d6e6a035615fea1e611cc5163c9dcec6cc21197f
mattssll992 <56899645+mattssll@users.noreply.github.com>
2022-05-25 20:48:28 -0700

    Time: 319 ms (46.78%), Space: 15.6 MB (22.75%) - LeetHub

---
+++ 704-binary-search/704-binary-search.py
@@ -0,0 +1,19 @@
+import math
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        begin = 0
+        end = len(nums) - 1
+        found = False
+        while begin <= end:
+            
+            midpoint = math.floor((begin + end) / 2)
+            
+            if nums[midpoint] > target:
+                end = midpoint - 1  
+            elif nums[midpoint] < target:
+                begin = midpoint + 1
+            else:  # nums[midpoint] == target
+                return midpoint
+        
+        return -1 # if not found
