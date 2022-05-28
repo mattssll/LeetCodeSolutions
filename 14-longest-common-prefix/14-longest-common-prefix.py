c4a29cf446656a9942a2132d60d98efddde9c80d
mattssll992 <56899645+mattssll@users.noreply.github.com>
2022-05-27 18:03:20 -0600

    Time: 40 ms (73.59%), Space: 14 MB (11.74%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -1,22 +1,22 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         len_smallest_string = len(min(strs, key=len))
-        number_of_words = len(strs)
+        number_of_elements = len(strs)
         same_prefix = True
         same_prefix_words = ""
         
         # handling edge cases:
         if strs == [""]:
             return ""
-        elif number_of_words == 1:
+        elif number_of_elements == 1:
             return strs[0][0]
         
         # handling regular case:
         for i in range(len_smallest_string):
             
-            for j in range(number_of_words):
+            for j in range(number_of_elements):
 
-                if j+1 == number_of_words:
+                if j+1 == number_of_elements:
                     break
                 elif strs[j][i] == strs[j+1][i]:
                     same_prefix = True
