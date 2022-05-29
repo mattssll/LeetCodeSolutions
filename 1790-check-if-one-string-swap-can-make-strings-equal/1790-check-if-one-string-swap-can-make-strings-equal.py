407ca438019519e6e8dd445f688ee2a0431115c5
mattssll992 <56899645+mattssll@users.noreply.github.com>
2022-05-29 12:26:40 -0600

    Time: 48 ms (38.72%), Space: 14 MB (21.83%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -2,7 +2,8 @@ class Solution:
     
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
     
-        def bool_distinct_letters(s1: str, s2: str) -> bool:
+        def bool_distinct_letters(s1: str, s2: str, is_sorted: bool) -> bool:
+            comparison_check_value = 0 if is_sorted else 2
             distinct_letters = 0
             for i in range(len(s1)):
 
@@ -11,35 +12,20 @@ class Solution:
                 else:
                     continue
 
-            if distinct_letters <= 2:
+            if distinct_letters <= comparison_check_value:
                 return True
             else:
                 return False
 
-        def sorted_distinct_letters(s1: [str], s2: [str]) -> bool:
-            distinct_letters = 0
-            for i in range(len(s1)):
-
-                if s1[i] != s2[i]:
-                    distinct_letters += 1
-                else:
-                    continue
-
-            if distinct_letters <= 0:
-                return True
-            else:
-                return False
-
-
-            if len(s1) != len(s2):
-                return False
         
-        distinct_letters = bool_distinct_letters(s1, s2)
-        sorted_distinct_letters = sorted_distinct_letters(sorted(s1), sorted(s2))
+        distinct_letters = bool_distinct_letters(s1, s2, False)
+        sorted_distinct_letters = bool_distinct_letters(sorted(s1), sorted(s2), True)
         
         if distinct_letters and sorted_distinct_letters:
             return True
         else:
             return False
+        
+        
         
         
