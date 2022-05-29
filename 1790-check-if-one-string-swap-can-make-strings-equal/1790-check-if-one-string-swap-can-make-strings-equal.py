450b506585de7890ba2830c168e2e2efbb2a4a5a
mattssll992 <56899645+mattssll@users.noreply.github.com>
2022-05-29 12:27:39 -0600

    Time: 35 ms (77.04%), Space: 13.9 MB (73.49%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -2,7 +2,7 @@ class Solution:
     
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
     
-        def bool_distinct_letters(s1: str, s2: str, is_sorted: bool) -> bool:
+        def checker(s1: str, s2: str, is_sorted: bool) -> bool:
             comparison_check_value = 0 if is_sorted else 2
             distinct_letters = 0
             for i in range(len(s1)):
@@ -18,10 +18,10 @@ class Solution:
                 return False
 
         
-        distinct_letters = bool_distinct_letters(s1, s2, False)
-        sorted_distinct_letters = bool_distinct_letters(sorted(s1), sorted(s2), True)
+        max_one_swap = checker(s1, s2, False)
+        no_different_letters = checker(sorted(s1), sorted(s2), True)
         
-        if distinct_letters and sorted_distinct_letters:
+        if max_one_swap and no_different_letters:
             return True
         else:
             return False
