7984089d93278603e52ecaa124a01745d6528a8e
mattssll992 <56899645+mattssll@users.noreply.github.com>
2022-05-30 13:17:35 -0600

    Time: 790 ms (22.97%), Space: 13.8 MB (95.98%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        # We'll solve this issue with recursion
+        if n <= 1:
+            return n
+        else:
+            return self.fib(n-1) + self.fib(n-2)
+            
+        return fibonnaci_fx(n)
+        
+        
+        
