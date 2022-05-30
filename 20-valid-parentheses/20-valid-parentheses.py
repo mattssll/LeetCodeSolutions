24beb05aac713be3db9b0f5dab3cb0c0e967b211
mattssll992 <56899645+mattssll@users.noreply.github.com>
2022-05-29 18:58:44 -0600

    Time: 50 ms (32.98%), Space: 13.9 MB (23.99%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        """
+        Opening should be followed by closing (of same item)
+        We handle two cases, when it's an opening item, we add to stack
+        when it's a closing item, we check the value in the lookup_dict,
+        and if it matches, then we remove from stack (cause it's good),
+        if not, then we return false and we say it's not valid
+        """
+        stack = []
+        lookup_dict = {
+            "}" : "{",
+            ")" : "(",
+            "]" : "["
+        }
+        
+        for char in s:
+        
+            if char in lookup_dict.values():
+                stack.append(char)
+            elif char in lookup_dict.keys():
+                if stack and stack[-1] == lookup_dict[char]:
+                    stack.pop()
+                else:
+                    return False
+            
+        if stack == []:
+            return True
+        
