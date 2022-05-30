2dd61f37981f0292082bd78289577b944cfc838e
mattssll992 <56899645+mattssll@users.noreply.github.com>
2022-05-29 19:00:59 -0600

    Time: 51 ms (30.66%), Space: 14 MB (23.99%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -15,7 +15,6 @@ class Solution:
         }
         
         for char in s:
-        
             if char in lookup_dict.values():
                 stack.append(char)
             elif char in lookup_dict.keys():
