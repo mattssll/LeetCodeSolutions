5384ee83c29cb313c21d326df1a44e64c2b51b7e
msslzlzl <m4ttssll@gmail.com>
2025-01-27 15:17:48 +0100

    Time: 3 ms (22.13%), Space: 18.6 MB (12.7%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,40 +1,29 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#class TreeNode:
+#    def __init__(self, val=0, left=None, right=None):
+#        self.val = val
+#        self.left = left
+#        self.right = right
+
+from typing import Optional, List
+
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        
-        self.visited = set()
-        self.queue = collections.deque()
-        self.res = []
-        self.treeBfs(root, 0)
+        self.res = []  # List to store the result
+        self.treeBfs(root, 0)  # Start the recursive BFS from the root at level 0
         return self.res
-        
     
-    def treeBfs(self, root, level):
-        if root is None:
-            return []
-        # add to queue and visited
-        self.queue.append(root)
-        self.visited.add(root)
-
-        while self.queue:
-            len_q = len(self.queue)
-            # here we append the values from each level
+    def treeBfs(self, node: TreeNode, level: int):
+        if node is None:
+            return
+        
+        # If this is the first time we've reached this level, we add a new sublist
+        if len(self.res) == level:
             self.res.append([])
-            
-            for _ in range(len_q):
-            # start dequeuing and go for neighbours
-                node = self.queue.popleft()
-                self.res[level].append(node.val)  # appending tree root
-                # handle left and right node
-                if node.left is not None:
-                    self.queue.append(node.left)
-                if node.right is not None:
-                    self.queue.append(node.right)
-            level+=1  # go to next level
         
+        # Add the current node's value to the corresponding level list
+        self.res[level].append(node.val)
         
+        # Recurse on left and right children with incremented level
+        self.treeBfs(node.left, level + 1)
+        self.treeBfs(node.right, level + 1)
