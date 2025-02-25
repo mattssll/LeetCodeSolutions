171219bd38868053aebccb460ed7544ba0fbd3b6
msslzlzl <m4ttssll@gmail.com>
2025-02-25 19:00:49 +0000

    Time: 1183 ms (5.09%), Space: 12.5 MB (51.83%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -4,11 +4,26 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        """
+        # naive recursive approach
         if n==0:
             f=0
         elif n==1:
             f=1
         else:
             f = self.fib(n-1)+self.fib(n-2)
+        return f
+        """
+        # dp memoization
+        memo = {}
+        if n in memo:
+            return memo[n] 
+        elif n==0:
+            f=0
+        elif n==1:
+            f=1
+        else:
+            f = self.fib(n-1)+self.fib(n-2)
+        memo[n] = f
         return f
         
