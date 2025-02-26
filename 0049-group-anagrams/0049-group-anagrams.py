3e6f526d01c7b39783d18f25a502b3461fa66037
msslzlzl <m4ttssll@gmail.com>
2025-02-26 21:26:41 +0000

    Time: 191 ms (8.4%), Space: 18 MB (30.66%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,31 @@
-class Solution:
-    # The idea is sorting the dict keys and then handling presence (add [word]) and absence (append to value)
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+class Solution(object):
+    def get_char_frequency(self, word):
+        frequency = {
+            letter: 0 for letter in 'abcdefghijklmnopqrstuvwxyz'
+        }  # Initialize for 'a' to 'z'
+        for letter in word:
+            frequency[letter] = frequency.get(letter, 0) + 1
+        return str(frequency)
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        res = []
+        frequency = {
+            letter: 0 for letter in 'abcdefghijklmnopqrstuvwxyz'
+        }  # Initialize for 'a' to 'z'
         res_dict = {}
-        # Iterate through each word in the input list
         for word in strs:
-            sorted_word = ''.join(sorted(word))
-            if sorted_word not in res_dict.keys():
-                res_dict[sorted_word] = [word]
-            # Generate a frequency dictionary for the word
+            w_freq = self.get_char_frequency(word)
+            if str(self.get_char_frequency(word)) not in res_dict:
+                res_dict[w_freq] = [word]
             else:
-                res_dict[sorted_word].append(word)
-        return list(res_dict.values())
+                res_dict[w_freq].append(word)
+        
+        for a_group in res_dict.values():
+            res.append(a_group)
+        
+        return res
+
+        
