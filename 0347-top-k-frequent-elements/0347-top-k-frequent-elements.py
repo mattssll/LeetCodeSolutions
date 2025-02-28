02491c9769e909579bccc6345a7bd379545f8b25
msslzlzl <m4ttssll@gmail.com>
2025-02-28 15:10:20 +0000

    Time: 15 ms (37.54%), Space: 20 MB (14.09%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,34 +5,22 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        # first solution can be used a hashmap,
-        # second a heapify
-        # better one is a variation of bucket sort
-        # 1. we add the num in the index of the count:
-        # if number 3 appears 3 times, we add it to index 3
         size = len(nums)
-        count_dict = {}
-        # should go from 1 to number of elements array (max count n can appear)
-        freq = [[] for i in range(size+1)]
+        count_dict = {}  # Dictionary to store the frequency of each number
+        freq = [[] for _ in range(size + 1)]  # List of lists for bucket sorting
         
-        # we need a dict with the count of each of the numbers
-        for n in nums:
-            count_dict[n] = count_dict.get(n, 0) + 1
+        # Count occurrences of each number
+        for num in nums:
+            count_dict[num] = count_dict.get(num, 0) + 1
         
-        # now we assign them numbers to the freq
-        # if 1 -> 3 (1 appear 3 times) then we add to index 3
-        # if n has count(i) -> then we add n to index(i)
-        for n, count in count_dict.items():
-            freq[count].append(n)        
-        print(freq)
-
-        # if we have k = 2 we need to return 2 most frequent
+        # Group numbers by their frequency
+        for num, count in count_dict.items():
+            freq[count].append(num)
+        
+        # Collect the top k frequent elements
         results = []
-        for i in range(len(freq)-1, 0, -1):  # len(freq)-1 because we want to start from latest index
-            for number in freq[i]:
-                results.append(number)
-                if len(results)==k:
+        for i in range(size, 0, -1):  # Start from the highest frequency
+            for num in freq[i]:
+                results.append(num)
+                if len(results) == k:  # Stop when we have k elements
                     return results
-
-
-        
