f7bfed07de3b4ef6b727244ec6d3fe3bd6d2f142
msslzlzl <m4ttssll@gmail.com>
2025-03-07 16:58:49 +0000

    Time: 8 ms (53.61%), Space: 18.8 MB (31.53%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,16 +1,16 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        s = ''.join(c.lower() for c in s if c.isalnum())
-        l=0
-        r=len(s)-1
-        equal = True
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = "".join(l for l in s if l.isalnum())
+        print(s)
+        l = 0
+        r = len(s)-1
+        res = False
         while l<r:
-            if s[l]!=s[r]:
-                return False
-            else:
+            if s[l]==s[r]:
                 l+=1
                 r-=1
-        
+                continue
+            else:
+                return False
         return True
-        
-        
